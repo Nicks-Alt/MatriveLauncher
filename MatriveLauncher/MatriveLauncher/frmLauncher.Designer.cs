613eb784e3a2ba1c4668dec28dd5998032558c1f
Nick <nicksuperiorservers@gmail.com>
2019-12-21 04:22:24 -0500

    Init

---
+++ MatriveLauncher/MatriveLauncher/frmLauncher.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MatriveLauncher
+{
+    partial class frmLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLauncher));
+            this.topBar = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblPlayerCount = new System.Windows.Forms.Label();
+            this.tmrPlayerCountRefresh = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tmrCurrentServerQuery = new System.Windows.Forms.Timer(this.components);
+            this.topBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // topBar
+            // 
+            this.topBar.BackColor = System.Drawing.Color.Black;
+            this.topBar.Controls.Add(this.btnClose);
+            this.topBar.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.topBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topBar.Location = new System.Drawing.Point(0, 0);
+            this.topBar.Name = "topBar";
+            this.topBar.Size = new System.Drawing.Size(575, 27);
+            this.topBar.TabIndex = 0;
+            this.topBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseDown);
+            this.topBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
+            this.topBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseUp);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(546, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(29, 27);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(151, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(272, 91);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(575, 101);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Gray;
+            this.btnConnect.FlatAppearance.BorderSize = 0;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Font = new System.Drawing.Font("PT Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(165, 127);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(244, 78);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // lblPlayerCount
+            // 
+            this.lblPlayerCount.AutoSize = true;
+            this.lblPlayerCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerCount.Font = new System.Drawing.Font("Prototype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCount.ForeColor = System.Drawing.Color.White;
+            this.lblPlayerCount.Location = new System.Drawing.Point(95, 207);
+            this.lblPlayerCount.Name = "lblPlayerCount";
+            this.lblPlayerCount.Size = new System.Drawing.Size(131, 33);
+            this.lblPlayerCount.TabIndex = 4;
+            this.lblPlayerCount.Text = "000/000";
+            // 
+            // tmrPlayerCountRefresh
+            // 
+            this.tmrPlayerCountRefresh.Enabled = true;
+            this.tmrPlayerCountRefresh.Interval = 1000;
+            this.tmrPlayerCountRefresh.Tick += new System.EventHandler(this.TmrPlayerCountRefresh_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(469, 155);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Location = new System.Drawing.Point(508, 155);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // tmrCurrentServerQuery
+            // 
+            this.tmrCurrentServerQuery.Enabled = true;
+            this.tmrCurrentServerQuery.Interval = 60000;
+            this.tmrCurrentServerQuery.Tick += new System.EventHandler(this.TmrCurrentServerQuery_Tick);
+            // 
+            // frmLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(61)))), ((int)(((byte)(82)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(575, 255);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblPlayerCount);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.topBar);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmLauncher";
+            this.topBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topBar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblPlayerCount;
+        private System.Windows.Forms.Timer tmrPlayerCountRefresh;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer tmrCurrentServerQuery;
+    }
+}
+
---
+++ MatriveLauncher/MatriveLauncher/frmLauncher.cs
@@ -0,0 +1,298 @@
+﻿using DiscordRPC;
+using DiscordRPC.Message;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatriveLauncher
+{
+    public partial class frmLauncher : Form
+    {
+        #region Global Vars
+        bool isTopPanelDragged = false;
+        bool isWindowMaximized = false;
+        Point offset;
+        Size _normalWindowSize;
+        Point _normalWindowLocation = Point.Empty;
+        private PrivateFontCollection fonts = new PrivateFontCollection();
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
+        private DiscordRPC.DiscordRpcClient discord = new DiscordRPC.DiscordRpcClient("657810158273036311");
+        #endregion
+
+        #region Handlers
+        public frmLauncher()
+        {
+            InitializeComponent();
+            TmrPlayerCountRefresh_Tick(this, new EventArgs()); // Get player count
+            byte[] fontData = Properties.Resources.Prototype;
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            uint dummy = 0;
+            fonts.AddMemoryFont(fontPtr, Properties.Resources.Prototype.Length);
+            AddFontMemResourceEx(fontPtr, (uint)Properties.Resources.Prototype.Length, IntPtr.Zero, ref dummy);
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+            btnConnect.Font = new Font(fonts.Families[0], btnConnect.Font.Size);
+            discord.Initialize();
+            discord.RegisterUriScheme("4000", "steam://rungameid/4000");
+            TmrCurrentServerQuery_Tick(this, new EventArgs());
+        }
+
+        private void TopBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            isTopPanelDragged = false;
+            if (this.Location.Y <= 5)
+            {
+                if (!isWindowMaximized)
+                {
+                    _normalWindowSize = this.Size;
+                    _normalWindowLocation = this.Location;
+
+                    Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+                    this.Location = new Point(0, 0);
+                    this.Size = new System.Drawing.Size(rect.Width, rect.Height);
+
+                    isWindowMaximized = true;
+                }
+            }
+        }
+
+        private void TopBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isTopPanelDragged)
+            {
+                Point newPoint = topBar.PointToScreen(new Point(e.X, e.Y));
+                newPoint.Offset(offset);
+                this.Location = newPoint;
+
+                if (this.Location.X > 2 || this.Location.Y > 2)
+                {
+                    if (this.WindowState == FormWindowState.Maximized)
+                    {
+                        this.Location = _normalWindowLocation;
+                        this.Size = _normalWindowSize;
+
+                        isWindowMaximized = false;
+                    }
+                }
+            }
+        }
+
+        private void TopBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isTopPanelDragged = true;
+                Point pointStartPosition = this.PointToScreen(new Point(e.X, e.Y));
+                offset = new Point
+                {
+                    X = this.Location.X - pointStartPosition.X,
+                    Y = this.Location.Y - pointStartPosition.Y
+                };
+            }
+            else
+            {
+                isTopPanelDragged = false;
+            }
+            if (e.Clicks == 2)
+            {
+                isTopPanelDragged = false;
+
+            }
+        }
+        private void TmrPlayerCountRefresh_Tick(object sender, EventArgs e)
+        {
+            lblPlayerCount.Text = GetPlayerCount("74.91.115.113").ToString() + "/" + GetMaxPlayerCount("74.91.115.113").ToString();
+            CenterControl(lblPlayerCount, this);
+        }
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/n7gNUWg");
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://matrive.net/forums/");
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            Process.Start("steam://connect/74.91.115.113:27015");
+        }
+        #endregion
+
+        #region Cool Methods and shit
+        /// <summary>
+        /// Aligns a specified control to the center of the parent control.
+        /// </summary>
+        /// <param name="ctrl">The control to center.</param>
+        /// <param name="parent">The control containing the control.</param>
+        private void CenterControl(Control ctrl, Control parent)
+        {
+            ctrl.Left = (parent.Size.Width / 2) - (ctrl.Width / 2); // Center Horizontally
+            //ctrl.Top = (parent.Size.Height / 2) - (ctrl.Height / 2); // Center vertically
+        }
+        private byte GetPlayerCount(string ip)
+        {
+            //  SERVERS:
+            // Rockford: 74.91.115.113
+            // More to come...hopefully.
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            byte[] rawData = new byte[512];
+            socket.Connect(ip, 27015);
+            byte[] sendBytes = { 0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 };
+            socket.Send(sendBytes);
+
+            socket.Receive(rawData);
+            using (var ms = new MemoryStream(rawData))
+            {
+                ms.ReadByte();
+                ms.ReadByte();
+                ms.ReadByte();
+                ms.ReadByte();
+
+                ms.ReadByte();
+                ms.ReadByte();
+
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+
+                ms.ReadByte();
+                ms.ReadByte();
+
+                return Convert.ToByte(ms.ReadByte());
+            }
+        }
+        private byte GetMaxPlayerCount(string ip)
+        {
+            //  SERVERS:
+            // Rockford: 74.91.115.113
+            // More to come...hopefully.
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            byte[] rawData = new byte[512];
+            socket.Connect(ip, 27015);
+            byte[] sendBytes = { 0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 };
+            socket.Send(sendBytes);
+
+            socket.Receive(rawData);
+            using (var ms = new MemoryStream(rawData))
+            {
+                ms.ReadByte();
+                ms.ReadByte();
+                ms.ReadByte();
+                ms.ReadByte();
+
+                ms.ReadByte();
+                ms.ReadByte();
+
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+                ms.ReadTerminatedString();
+
+                ms.ReadByte();
+                ms.ReadByte();
+                ms.ReadByte();
+
+                return Convert.ToByte(ms.ReadByte());
+            }
+        }
+        private bool GetPlayerServerStatus()
+        {
+            string steamID = new SteamBridge().GetSteamId().ToString();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // Secure security protocol for querying the steam API
+            HttpWebRequest request = WebRequest.CreateHttp("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=51F8018514A1DFC912FBED2375F1E9E1&steamids=" + steamID);
+            request.UserAgent = "Matrive";
+            WebResponse response = null;
+            response = request.GetResponse(); // Get Response from webrequest
+            StreamReader sr = new StreamReader(response.GetResponseStream()); // Create stream to access web data
+            var rawResults = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(sr.ReadToEnd());
+            if (!rawResults.response.players.First.ToString().Contains("gameserverip"))
+                return false;
+            string ip = rawResults.response.players.First.gameserverip.ToString();
+            //string playerName = rawResults.response.players.First.personaname.ToString();
+            if (ip != "74.91.115.113:27015")
+                return false;
+            else
+                return true;
+        }
+        #endregion
+
+
+        #region Discord Shit
+
+        private void TmrCurrentServerQuery_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show(discord.SteamID);
+            if (GetPlayerServerStatus())
+            {
+                discord.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "100% Custom",
+                    State = "In-Game",
+                    Timestamps = DiscordRPC.Timestamps.Now,
+                    Assets = new DiscordRPC.Assets()
+                    {
+                        LargeImageKey = "matrive",
+                        LargeImageText = "Matrive.net"
+                    }
+                });
+            }
+            else
+            {
+                discord.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = "100% Custom",
+                    State = "Idle",
+                    Timestamps = DiscordRPC.Timestamps.Now,
+                    Assets = new DiscordRPC.Assets()
+                    {
+                        LargeImageKey = "matrive",
+                        LargeImageText = "Matrive.net"
+                    }
+                });
+            }
+        }
+        #endregion
+    }
+}
+
+#region Extensions
+public static class MemoryStreamExtensions
+{
+    public static string ReadTerminatedString(this MemoryStream ms)
+    {
+        List<byte> res = new List<byte>();
+
+        byte last;
+        while ((last = (byte)ms.ReadByte()) != 0x00)
+        {
+            res.Add(last);
+        }
+
+        return System.Text.Encoding.ASCII.GetString(res.ToArray());
+    }
+}
+#endregion
